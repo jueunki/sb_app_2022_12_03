d7e110d3babef1238c374734eedaf20dd72acc5f
jueunki <inyeong1233@naver.com>
2022-12-03 23:07:08 +0900

    세팅

---
+++ src/main/java/com/sbs/exam/sb_app_2022_1203/SbApp20221203Application.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app_2022_1203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApp20221203Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApp20221203Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/exam/sb_app_2022_1203/SbApp20221203ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app_2022_1203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApp20221203ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
