6ee6641462579258e1795d7bf2477034d4b7f218
jueunki <inyeong1233@naver.com>
2022-12-07 22:22:20 +0900

    count 조절 액션

--- src/main/java/com/sbs/exam/sb_app_2022_1203/home/controller/UserHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_1203/home/controller/UserHomeController.java
@@ -6,38 +6,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
-  private int count;
+  int count;
 
   public UserHomeController(){
     count = -1;
   }
 
-  @RequestMapping("/user/home/main")
+  @RequestMapping("/user/home/getCount")
   @ResponseBody
-  public String showMain(){
-    return "hi!";
-  }
-  @RequestMapping("/user/home/main2")
-  @ResponseBody
-  public String showMain2(){
-    return "반가워!";
-  }
-  @RequestMapping("/user/home/main3")
-  @ResponseBody
-  public String showMain3(){
-    return "잘가!";
-  }
-  @RequestMapping("/user/home/main4")
-  @ResponseBody
-  public int showMain4(){
-    count++;
+  public int getCount(){
+
     return count;
   }
-  @RequestMapping("/user/home/main5")
+
+  @RequestMapping("/user/home/doSetCount")
   @ResponseBody
-  public String showMain5(){
-    count = 0;
-    return "count의 값이 0으로 초기화 되었습니다";
+  public String doSetCount(int count){
+    this.count = count;
+    return "count의 값이 " + this.count+ "으로 초기화 되었습니다";
   }
 
 }
