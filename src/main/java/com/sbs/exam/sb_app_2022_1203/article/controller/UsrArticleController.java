b8599e0ff1ac877a7984fbb7feedebfdeb42124e
jueunki <inyeong1233@naver.com>
2022-12-08 22:37:35 +0900

    UserArticleController가 가지고 있던 서비스 로직을 ArticleService로 옮김

--- src/main/java/com/sbs/exam/sb_app_2022_1203/article/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_1203/article/controller/UsrArticleController.java
@@ -7,78 +7,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class UsrArticleController {
-  // 인스턴스 변수 시작
-  @Autowired  //컴포넌트로 등록되는 것들에게 붙여주면 된다.
+  @Autowired
   private ArticleService articleService;
-  int articleLastId;
-
-  // 인스턴스 변수 끝
-
-  // 생성자
-
   private List<Article> articles;
 
-  public UsrArticleController() {
-    articles = new ArrayList<>();
-    articleLastId = 0;
 
-    makeTestDate();
-  }
-
-  // 서비스 메서드 시작
-  private void makeTestDate() {
-
-    for (int i = 1; i <= 10; i++) {
-      String title = "제목" + i;
-      String body = "내용" + i;
-
-      writeArticle(title, body);
-    }
-  }
-
-  private Article getArticle(int id) {
-
-    for (Article article : articles) {
-      if (article.getId() == id) {
-        return article;
-      }
-    }
-    return null;
-  }
-
-  private Article writeArticle(String title, String body) {
-    int id = articleLastId + 1;
-    Article article = new Article(id, title, body);
-
-    articles.add(article);
-    articleLastId = id;
-    return article;
-  }
-
-  private void deleteArticle(int id) {
-    Article article = getArticle(id);
-    articles.remove(article);
-  }
-
-  private void ModifyArticle(int id, String title, String body) {
-    Article article = getArticle(id);
-
-    article.setTitle(title);
-    article.setBody(body);
-
-  }
-  // 서비스 메서드 끝
-
-
+  // 액션 메서드 시작
   @RequestMapping("/user/article/doAdd")
   @ResponseBody
   public Article doAdd(String title, String body) {
-    Article article = writeArticle(title, body);
+    Article article = articleService.writeArticle(title, body);
     return article;
   }
 
@@ -86,13 +28,13 @@ public class UsrArticleController {
   @RequestMapping("/user/article/getArticles")
   @ResponseBody
   public List<Article> getArticles() {
-    return articles;
+    return articleService.getArticles();
   }
 
   @RequestMapping("/user/article/getArticle")
   @ResponseBody
   public Object getArticleAction(int id) { //상세보기는 하나만 가져오는것이기 때문에 복수와 단수를 정확하게 표현해줘야한다.
-    Article article = getArticle(id);
+    Article article = articleService.getArticle(id);
 
     if (article == null) {
       return id + "번 게시물이 존재하지 않습니다.";
@@ -105,12 +47,12 @@ public class UsrArticleController {
   @RequestMapping("/user/article/doDelete")
   @ResponseBody
   public String doDelete(int id) {
-    Article article = getArticle(id);
+    Article article = articleService.getArticle(id);
     if (article == null) {
       return id + "번 게시물이 존재하지 않습니다.";
     }
 
-    deleteArticle(id);
+    articleService.deleteArticle(id);
 
     return id + "번 게시물을 삭제하였습니다.";
   }
@@ -118,13 +60,13 @@ public class UsrArticleController {
   @RequestMapping("/user/article/doModify")
   @ResponseBody
   public String doModify(int id, String title, String body) {
-    Article article = getArticle(id);
+    Article article = articleService.getArticle(id);
 
     if (article == null) {
       return id + "번 게시물이 존재하지 않습니다.";
     }
 
-    ModifyArticle(id, title, body);
+    articleService.ModifyArticle(id, title, body);  //여기에 요청을 넣는것이다.
 
     return id + "번 게시물을 수정하였습니다."; //localhost:8081/user/article/doModify?id=1&title=제목1 수정&body=내용1 수정 이라고적으면 수정이 된다.
   }
