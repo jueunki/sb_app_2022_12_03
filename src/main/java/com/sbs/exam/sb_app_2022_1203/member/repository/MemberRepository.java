1d6777fedf82898f0f2c89d05fc2c99f38bf840f
jueunki <inyeong1233@naver.com>
2022-12-12 19:54:36 +0900

    MemberRepository추가, 회원가입 처리

--- src/main/java/com/sbs/exam/sb_app_2022_1203/member/repository/MemberRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_1203/member/repository/MemberRepository.java
@@ -1,4 +1,24 @@
 package com.sbs.exam.sb_app_2022_1203.member.repository;
 
-public class MemberRepository {
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberRepository {
+  @Insert("""
+          INSERT INTO `member`
+          SET regDate = NOW(),
+          updateDate = NOW(),
+          loginId = #{loginId},
+          loginPw = #{loginPw},
+          `name` = #{name},
+          `nickname` =#{nickname},
+          cellphoneNo = #{cellphoneNo},
+          email = #{email}
+          """)
+  public void join(@Param("loginId") String loginId,@Param("loginPw") String loginPw,@Param("name") String name,
+                   @Param("nickname") String nickname,@Param("cellphoneNo") String cellphoneNo,@Param("email") String email);
+
+
 }
--- src/main/java/com/sbs/exam/sb_app_2022_1203/member/service/MemberService.java
+++ src/main/java/com/sbs/exam/sb_app_2022_1203/member/service/MemberService.java
@@ -1,12 +1,20 @@
 package com.sbs.exam.sb_app_2022_1203.member.service;
 
+import com.sbs.exam.sb_app_2022_1203.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-  //private MemberRepository memberRepository;
+
+  private MemberRepository memberRepository; //@Autowired하면 안된다.
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+
 
   public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-    //memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+    memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
   }
 }
