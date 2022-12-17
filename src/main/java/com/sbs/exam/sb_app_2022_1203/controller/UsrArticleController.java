3c971bcce6944264cacb35816d13b9faebe83586
jueunki <inyeong1233@naver.com>
2022-12-17 21:52:44 +0900

    게시물 수정시 권한 체크

--- src/main/java/com/sbs/exam/sb_app_2022_1203/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_1203/controller/UsrArticleController.java
@@ -25,21 +25,21 @@ public class UsrArticleController {
     boolean isLogined = false;
     int loginedMemberId = 0;
 
-    if(httpSession.getAttribute("loginedMemberId") == null ) {
+    if (httpSession.getAttribute("loginedMemberId") == null) {
       // 로그인 되어있는 상태
       isLogined = true;
-      loginedMemberId = (int)httpSession.getAttribute("loginedMemberId");
+      loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
     }
-    if(isLogined == false) {
-      return ResultData.from("F-A","로그인 후 이용해주세요.");
-    }
-
-    if(Ut.empty(title)) {
-      return ResultData.from("F-1","title(을)를 입력해주세요.");
+    if (isLogined == false) {
+      return ResultData.from("F-A", "로그인 후 이용해주세요.");
     }
 
-    if(Ut.empty(body)) {
-      return ResultData.from("F-2","body(을)를 입력해주세요.");
+    if (Ut.empty(title)) {
+      return ResultData.from("F-1", "title(을)를 입력해주세요.");
+    }
+
+    if (Ut.empty(body)) {
+      return ResultData.from("F-2", "body(을)를 입력해주세요.");
     }
     // ResultData<Integer> 의 뜻 : 제너릭을 재정의하는것?
     // <Integer>를 쓴 이유 : 데이터 타입이 int인것을 return할것인데, int로 형변환을 해줄 필요가 없게된다.
@@ -68,10 +68,10 @@ public class UsrArticleController {
 
     if (article == null) {
 
-      return ResultData.from( "F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+      return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
     }
 
-    return ResultData.from( "S-1", Ut.f("%d번 게시물 입니다.", id), article);
+    return ResultData.from("S-1", Ut.f("%d번 게시물 입니다.", id), article);
     //String data type과 Int data type을 둘다 허용 하려면 둘의 상위type인 Object로 사용해준다(별로 좋은 방법은 아닙니다.)
   }
 
@@ -81,42 +81,63 @@ public class UsrArticleController {
     boolean isLogined = false;
     int loginedMemberId = 0;
 
-    if(httpSession.getAttribute("loginedMemberId") == null ) {
+    if (httpSession.getAttribute("loginedMemberId") != null) {
       // 로그인 되어있는 상태
       isLogined = true;
-      loginedMemberId = (int)httpSession.getAttribute("loginedMemberId");
+      loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
     }
-    if(isLogined == false) {
-      return ResultData.from("F-A","로그인 후 이용해주세요.");
+    if (isLogined == false) {
+      return ResultData.from("F-A", "로그인 후 이용해주세요.");
     }
     Article article = articleService.getArticle(id);
 
-    if(article.getMemberId() != loginedMemberId) {
-      return ResultData.from("F-2","권한이 없습니다.");
+    if (article.getMemberId() != loginedMemberId) {
+      return ResultData.from("F-2", "권한이 없습니다.");
     }
 
     if (article == null) {
-      return ResultData.from( "F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+      return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 
     }
 
     articleService.deleteArticle(id);
 
-    return ResultData.from( "S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id), id);
+    return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id), id);
   }
 
   @RequestMapping("/user/article/doModify")
   @ResponseBody
-  public ResultData<Integer> doModify(int id, String title, String body) {
+  public ResultData<Article> doModify(HttpSession httpSession, int id, String title, String body) {
+    boolean isLogined = false; //변수가 isLogined라는게 체킹되려면 이러한 변수를 셋팅해줘야한다.
+    int loginedMemberId = 0;
+
+    if (httpSession.getAttribute("loginedMemberId") != null) {
+      // 로그인 되어있는 상태
+      isLogined = true;
+      loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+    }
+    if (isLogined == false) {
+      return ResultData.from("F-A", "로그인 후 이용해주세요.");
+    }
     Article article = articleService.getArticle(id);
 
-    if (article == null) {
-      return ResultData.from( "F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+    if (article.getMemberId() != loginedMemberId) {
+      return ResultData.from("F-2", "권한이 없습니다."); //월권이기 때문에 서비스에게 넘기는것이 좋다.
     }
 
-    articleService.ModifyArticle(id, title, body);  //여기에 요청을 넣는것이다.
+    if (article == null) {
+      return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+    }
 
-    return ResultData.from( "S-1", Ut.f("%d번 게시물을 수정하였습니다.", id), id); //localhost:8081/user/article/doModify?id=1&title=제목1 수정&body=내용1 수정 이라고적으면 수정이 된다.
+    ResultData actorCanModifyRd = articleService.actorCanModify(loginedMemberId, article); //loginedMemberId가 article을 수정할 수 있다는 뜻
+
+    if (actorCanModifyRd.isFail()) {  //
+      return actorCanModifyRd;
+    }
+    //여기에 요청을 넣는것이다.
+
+    return articleService.modifyArticle(id, title, body); //localhost:8081/user/article/doModify?id=1&title=제목1 수정&body=내용1 수정 이라고적으면 수정이 된다.
+    //여기까지 오면 수정이 가능하다는것.
   }
 
 
