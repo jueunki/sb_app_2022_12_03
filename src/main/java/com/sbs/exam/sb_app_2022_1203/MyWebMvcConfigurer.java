aa2434012ca3f453f91afc7f85f94cb6e0395ecd
jueunki <inyeong1233@naver.com>
2022-12-21 15:48:16 +0900

    NeedLogin interceptor 구현 및 등록

--- src/main/java/com/sbs/exam/sb_app_2022_1203/MyWebMvcConfigurer.java
+++ src/main/java/com/sbs/exam/sb_app_2022_1203/MyWebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.sbs.exam.sb_app_2022_1203;
 
 import com.sbs.exam.sb_app_2022_1203.interceptor.BeforeActionInterceptor;
+import com.sbs.exam.sb_app_2022_1203.interceptor.NeedLoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,11 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // 프로그램이 처음 실행될때 처음 한번 읽는다.
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
   // beforeActionInterceptor 인터셉터 불러오기
   @Autowired
   BeforeActionInterceptor beforeActionInterceptor;
 
-
+  // NeedLoginInterceptor 인터셉터 불러오기
+  @Autowired
+  NeedLoginInterceptor needLoginInterceptor;
 
   // 이 함수는 인터셉터를 적용하는 역할을 합니다.
   @Override
@@ -24,5 +28,12 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
         .excludePathPatterns("/js/**")  //->js는 interceptor가 작동 할 필요가 없는 작업이다.
                                         //localhost:8081/css/common.css라고 하면 출력된다.
         .excludePathPatterns("/error");
+
+    registry.addInterceptor(needLoginInterceptor)
+        .addPathPatterns("/user/article/write")   //글 작성폼
+        .addPathPatterns("/user/article/doWrite") //글 전송폼
+        .addPathPatterns("/user/article/modify")
+        .addPathPatterns("/user/article/doModify")
+        .addPathPatterns("/user/article/doDelete");
   }
 }
--- src/main/java/com/sbs/exam/sb_app_2022_1203/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/sbs/exam/sb_app_2022_1203/interceptor/NeedLoginInterceptor.java
@@ -1,6 +1,5 @@
 package com.sbs.exam.sb_app_2022_1203.interceptor;
 
-import com.sbs.exam.sb_app_2022_1203.vo.Rq;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -9,12 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 
 //Rq 매번 적기 번거로워서 여기 interceptor를 만들어놓은것이다.
 @Component
-public class BeforeActionInterceptor implements HandlerInterceptor {
+public class NeedLoginInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handle) throws Exception {
-    System.out.println("로그인 필요");  // 리스트 버튼 누르면 실행되는부분.
-    Rq rq = new Rq(req);
-    req.setAttribute("rq", rq);
+    System.out.println("로그인 필요.");  // 리스트 버튼 누르면 실행되는부분.
+
 
     return HandlerInterceptor.super.preHandle(req, resp, handle);
   }
